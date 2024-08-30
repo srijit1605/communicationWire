c6b8a2f56fcdb679be76cdc64dec19b4db2f81e6
Srijit Ghosh <srijit16thmay@gmail.com>
2024-08-30 15:29:29 +0530

    baby steps

--- news-wire/app/page.js
+++ news-wire/app/page.js
@@ -1,15 +1,14 @@
+import Header from "@/components/Header/Header";
 import styles from "./page.module.css";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>Breaking News</div>
-      <div><div>date time</div><div>Social</div></div>
-      <div><div>left quote</div><div>NewsWire</div><div>right quote</div></div>
-      <div>nav menu</div>
+      <Header/>
       <div><div><div>left highlights</div><div><div>feature news</div><div>related widget</div></div></div><div>right widget</div></div>
       <div><div>left hand widgets section</div><div>right hand widget</div></div>
-      <div>footer</div>
+      <Footer/>
     </main>
   );
 }
---
+++ news-wire/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div>Breaking News</div>
+      <div><div>date time</div><div>Social</div></div>
+      <div><div>left quote</div><div>NewsWire</div><div>right quote</div></div>
+      <div>nav menu</div>
+      </>
+  )
+}
+
+export default Header
