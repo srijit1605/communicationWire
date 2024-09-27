95ef28f15ddb2bc439a4644433fa745c3576ff15
Srijit Ghosh <srijit16thmay@gmail.com>
2024-09-28 00:55:51 +0530

    styling modifications

--- news-wire/components/BottomWidgets/BottomWidget.jsx
+++ news-wire/components/BottomWidgets/BottomWidget.jsx
@@ -4,11 +4,52 @@ import SpotlightNews from '../NewsPreviews/SpotlightNews/SpotlightNews'
 import ImageCard from './ImageCard/ImageCard'
 
 const BottomWidget = () => {
+  const articles = [{
+    title: 'News Article Title',
+    body: 'This is the body of the news article. This is only for test purposes. To Reiterate, this article, as should be pretty obvious by now, is not an actual news article.',
+    author: 'someone',
+    date: 'Sep 5 2024',
+    comments: '1',
+    categories: 'Technology'
+},
+{
+    title: 'News Article Title',
+    body: 'This is the body of the news article. This is only for test purposes. To Reiterate, this article, as should be pretty obvious by now, is not an actual news article.',
+    author: 'someone',
+    date: 'Sep 5 2024',
+    comments: '1',
+    categories: 'Technology'
+},
+{
+    title: 'News Article Title',
+    body: 'This is the body of the news article. This is only for test purposes. To Reiterate, this article, as should be pretty obvious by now, is not an actual news article.',
+    author: 'someone',
+    date: 'Sep 5 2024',
+    comments: '1',
+    categories: 'Technology'
+},
+{
+    title: 'News Article Title',
+    body: 'This is the body of the news article. This is only for test purposes. To Reiterate, this article, as should be pretty obvious by now, is not an actual news article.',
+    author: 'someone',
+    date: 'Sep 5 2024',
+    comments: '1',
+    categories: 'Technology'
+},
+{
+    title: 'News Article Title',
+    body: 'This is the body of the news article. This is only for test purposes. To Reiterate, this article, as should be pretty obvious by now, is not an actual news article.',
+    author: 'someone',
+    date: 'Sep 5 2024',
+    comments: '1',
+    categories: 'Technology'
+}]
+
   return (
     <div>
         <SnippetNews/>
         <ImageCard/>
-        <SpotlightNews/>
+        <SpotlightNews title={'Local News'} articles={articles}/>
     </div>
   )
 }
--- news-wire/components/RightHandWidgets/RightHandWidget.jsx
+++ news-wire/components/RightHandWidgets/RightHandWidget.jsx
@@ -5,11 +5,53 @@ import ImageSlider from './ImageSlider/ImageSlider'
 import SnippetTab from './SnippetTab/SnippetTab'
 
 const RightHandWidget = () => {
+  const articles = [{
+    title: 'News Article Title',
+    body: 'This is the body of the news article. This is only for test purposes. To Reiterate, this article, as should be pretty obvious by now, is not an actual news article.',
+    author: 'someone',
+    date: 'Sep 5 2024',
+    comments: '1',
+    categories: 'Technology'
+},
+{
+    title: 'News Article Title',
+    body: 'This is the body of the news article. This is only for test purposes. To Reiterate, this article, as should be pretty obvious by now, is not an actual news article.',
+    author: 'someone',
+    date: 'Sep 5 2024',
+    comments: '1',
+    categories: 'Technology'
+},
+{
+    title: 'News Article Title',
+    body: 'This is the body of the news article. This is only for test purposes. To Reiterate, this article, as should be pretty obvious by now, is not an actual news article.',
+    author: 'someone',
+    date: 'Sep 5 2024',
+    comments: '1',
+    categories: 'Technology'
+},
+{
+    title: 'News Article Title',
+    body: 'This is the body of the news article. This is only for test purposes. To Reiterate, this article, as should be pretty obvious by now, is not an actual news article.',
+    author: 'someone',
+    date: 'Sep 5 2024',
+    comments: '1',
+    categories: 'Technology'
+},
+{
+    title: 'News Article Title',
+    body: 'This is the body of the news article. This is only for test purposes. To Reiterate, this article, as should be pretty obvious by now, is not an actual news article.',
+    author: 'someone',
+    date: 'Sep 5 2024',
+    comments: '1',
+    categories: 'Technology'
+}]
+
+
   return (
     <div>
       <SnippetTab/>
       <ImageSlider/>
-      <SpotlightNews/>
+      <SpotlightNews  title={'Fast Facts'} articles={articles}/>
       <SnippetNews/>
     </div>
   )
