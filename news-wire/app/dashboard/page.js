674992f9933d5c07064fd049f2e379b5dfea0003
Srijit Ghosh <srijit16thmay@gmail.com>
2024-10-01 19:22:37 +0530

    profile dropdown and logout option implemented. styling fix required.

--- news-wire/app/dashboard/page.js
+++ news-wire/app/dashboard/page.js
@@ -1,5 +1,7 @@
 'use client';
+import styles from "@/app/page.module.css";
 import AddFeedForm from '@/components/AddFeedForm/AddFeedForm';
+import ProfileDropdownMenu from '@/components/ProfileDropdownMenu/ProfileDropdownMenu';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
@@ -21,7 +23,8 @@ export default function Dashboard() {
   }
 
   return (
-    <div>
+    <div className={styles.main}>
+      <div style={{position: 'absolute', top: '250px', right: '40px', filter: 'drop-shadow(2px 2px 2px #666)'}}><ProfileDropdownMenu/></div>
       <h1>Welcome to your Dashboard</h1>
       {/* You can display user info here */}
       <p>Logged in as: {session?.user?.email}</p>
--- news-wire/app/layout.js
+++ news-wire/app/layout.js
@@ -15,11 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      
+      <Providers>
         <Header/>
-        <main><Providers>{children}</Providers></main>
+        <main>{children}</main>
         <Footer/>
-        
+      </Providers>
         </body>
     </html>
   );
