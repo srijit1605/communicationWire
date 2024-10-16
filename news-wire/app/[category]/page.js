a4c0b5dc4ee3fe9c64b6686a490d1511c2f0174e
Srijit Ghosh <srijit16thmay@gmail.com>
2024-10-16 14:22:38 +0530

    category page work started

--- news-wire/app/[category]/page.js
+++ news-wire/app/[category]/page.js
@@ -1,8 +1,27 @@
+"use client"
+
 import React from 'react'
+import styles from "../page.module.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import LeftHandWidget from '@/components/LeftHandWidgets/LeftHandWidget';
+import RightHandWidget from '@/components/RightHandWidgets/RightHandWidget';
 
 const page = () => {
   return (
-    <div>Category page</div>
+    <div className={styles.main}>Category page
+    <Container className={styles.heroContainerAlignment}>
+    <Row className={styles.rowGutter}>
+          <Col sm={12} md={8} >
+          <LeftHandWidget/>
+          </Col>
+          <Col sm={12} md={4}>
+          <RightHandWidget/>
+          </Col>
+        </Row>
+    </Container>
+    </div>
   )
 }
 
