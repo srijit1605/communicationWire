da3028beee26f2b29b2c8a19051b2729b2365a46
Srijit Ghosh <srijit16thmay@gmail.com>
2024-10-26 01:42:45 +0530

    minor update

--- news-wire/components/Footer/Footer.jsx
+++ news-wire/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
+      <div style={{display: 'none'}}>if this is the only push today, then it kind of feels like cheating</div>
       <a href="https://www.flaticon.com/free-icons/facebook" title="facebook icons">Facebook icons created by Freepik - Flaticon</a>
       <a href="https://www.flaticon.com/free-icons/instagram-logo" title="instagram logo icons">Instagram logo icons created by Freepik - Flaticon</a>
       <a href="https://www.flaticon.com/free-icons/tweet" title="tweet icons">Tweet icons created by Freepik - Flaticon</a>
